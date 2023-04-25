e4519059ee544ab855b6bfb27c38a6c07915b9cd
kafka <jjk5158@naver.com>
2023-04-25 16:59:28 +0900

    :art: REFACTOR : 예외처리 - 비밀번호 검증 보완

--- src/main/java/kafka/boardproject/sys/dto/SignupRequsetDto.java
+++ src/main/java/kafka/boardproject/sys/dto/SignupRequsetDto.java
@@ -17,7 +17,7 @@ public class SignupRequsetDto {
     private String username;
     @NotBlank(message = "비밀번호는 필수 입력입니다.")
     @Size(min = 8, max = 15, message = "비밀번호는 8자이상 15자 이하를 입력하셔야 합니다.")
-//    @Pattern(regexp =" ^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,15}$", message = "비밀번호는 알파벳  대소문자(a~z, A~Z), 숫자(0~9), 특수문자로 구성되어야 합니다.")
+    @Pattern(regexp ="^(?=.*[a-zA-Z])(?=.*\\d)(?=.*\\W).{8,15}$", message = "비밀번호는 알파벳  대소문자(a~z, A~Z), 숫자(0~9), 특수문자로 구성되어야 합니다.")
     private String password;
 
     private UserRoleEnum role;
